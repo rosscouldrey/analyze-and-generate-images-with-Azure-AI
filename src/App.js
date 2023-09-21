3bf5fd5955761a7cb8675c0eff842f556c2aff7e
Ross Couldrey <roscoesemail@gmail.com>
2023-09-21 13:53:50 -0400

    updating webpage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+function text(text){
+  return(
+    <p> {text} </p> 
+  )
+}
+
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+  <div>
+    <h1> My Test App  </h1>
+    <text text="Hello World" />
+    <MyButton />
+  </div>
+
+
+  )
 }
 
 export default App;
